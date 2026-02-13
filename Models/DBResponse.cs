cadf2e950ebd5b350ef3c80db078ba17042079a1
AVRAJYOTI DUTTA <avrajyoti.dutta@agh.edu.pl>
2026-02-13 16:50:19 +0100

    Initial COmmit

---
+++ Models/DBResponse.cs
@@ -0,0 +1,13 @@
+﻿using MySqlConnector;
+using System.Data;
+
+namespace IQA.Models
+{
+    public sealed class MySqlResponse
+    {
+        public DataSet? Data { get; init; }
+        public int? RowsAffected { get; init; }
+        public List<MySqlParameter?> OutputParameters { get; init; } = new();
+    }
+
+}
